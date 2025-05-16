bbef8eb7f5ea68d3d2b8aa18faa68d9879e449ef
_1ms <106094224+GRX005@users.noreply.github.com>
2025-05-16 18:08:37 +0200

    Base

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,7 @@
-fn main() {
-    println!("Starting McModManager.");
-    let szam = Num {num1:24,num2:75};
-    println!("{}",szam.add());
-    println!("{}",Cucc(szam));
-    let sz = Num2 {num1:64.321134,num2:13.438865};
-    print!("{}",Cucc(sz));
-}
+slint::include_modules!();
 
-fn Cucc(arg: impl Calc)->i64 {
-    arg.mult()
-}
+fn main() -> Result<(), slint::PlatformError> {
+    let main_window = MainWindow::new()?;
 
-struct Num {
-    num1:i64,
-    num2:i64,
-}
-
-struct Num2 {
-    num1:f64,
-    num2:f64,
-}
-
-impl Num {
-    fn add(&self)->i64 {
-        self.num1+self.num2
-    }
-}
-
-trait Calc {
-    fn mult(&self) -> i64;
-}
-
-impl Calc for Num {
-    fn mult(&self) -> i64 {
-        self.num1*self.num2
-    }
-}
-
-impl Calc for Num2 {
-    fn mult(&self) -> i64 {
-        (self.num2*self.num1) as i64
-    }
+    main_window.run()
 }
