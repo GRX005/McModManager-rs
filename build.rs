d7d57345d15944507d77c2d74ab289dfb35dbfc4
_1ms <106094224+GRX005@users.noreply.github.com>
2025-05-17 14:29:54 +0200

    Build config

--- build.rs
+++ build.rs
@@ -1,3 +1,16 @@
 fn main() {
-    slint_build::compile("ui/mainWindow.slint").expect("Slint build failed");
+    slint_build::compile("ui/mainWindow.slint").expect("Slint build failed"); 
+    //Windows specific:
+    println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS,10.0");
+    let mut res = winres::WindowsResource::new();
+    res.set("FileDescription", "Minecraft Mod Manager");
+    res.set("CompanyName", "_1ms");
+    res.set("ProductName", "McModManager");
+    res.set("LegalCopyright", "Copyright © 2025 _1ms");
+    // Use your Cargo version or hardcode a string
+    res.set("FileVersion", env!("CARGO_PKG_VERSION"));
+    res.set("ProductVersion", env!("CARGO_PKG_VERSION"));
+
+    // 3) Compile and link the resource into the EXE
+    res.compile().expect("failed to compile resources");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
